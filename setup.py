7cf133f85b35cc1a52945c6d4efc8722609f5e92
Ivan Popovych <ivan.popovich000@gmail.com>
2022-02-15 18:46:24 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="mono2ledger",
+    version="1.0",
+    description="Utility to convert monobank card statement to ledger entries",
+    author="Ivan Popovych",
+    url="https://github.com/ivan-pop/mono2ledger",
+    scripts=["mono2ledger"],
+)
